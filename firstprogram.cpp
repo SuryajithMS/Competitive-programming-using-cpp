bf768749fe61ab45af1c6648a1cedb4d94710e94
Suryajith M S <76899256+SuryajithMS@users.noreply.github.com>
2022-12-28 15:07:29 +0530

    Add files via upload

---
+++ firstprogram.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int amount1,amount2;
+    cout<<"enter amount1 \n";
+     cin>>amount1;
+   
+    cout<<"enter amount2 \n";
+     cin>>amount2;
+    int sum= amount1+amount2;
+    cout<<"total amount is"<<endl;
+    cout<<sum;
+    
+
+    return 0;
+}
